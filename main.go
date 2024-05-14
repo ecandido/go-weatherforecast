b21e5108c068e9dfcff7ed08c89bbe77eefee46f
Eduardo Candido Goncalves <e_candido@live.com>
2024-05-14 20:19:11 -0300

    feat: main com declaração do srv e controller do weatherforecast

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
-import {
+import (
+	"go-weatherforecast/controllers"
+	"log"
 	"net/http"
-}
+)
 
-func main(){
+func main() {
+	http.HandleFunc("/weatherforecast", controllers.GetWeatherForecast)
 
+	log.Println("Server starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
